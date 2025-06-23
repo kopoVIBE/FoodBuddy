a9762628eb51afb45816ed896fa8500ed0759d20
b4yen <yeeunjeon1@naver.com>
2025-06-23 16:24:44 +0900

    fix: 영수증

--- BE/yoriview/src/main/java/com/vibe/yoriview/domain/receipt/dto/ReceiptResponseDto.java
+++ BE/yoriview/src/main/java/com/vibe/yoriview/domain/receipt/dto/ReceiptResponseDto.java
@@ -15,6 +15,7 @@ public class ReceiptResponseDto {
     private String receiptId;
     private String userId;
     private String restaurantId;
+    private String restaurantName; // OCR로 추출한 상호명 필드 추가
     private String originalImg;
     private LocalDate receiptDate;
     private String receiptAddress;
